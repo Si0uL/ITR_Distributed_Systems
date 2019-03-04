c4e1ce4bb44755a6c44ee98a5976aae754d79b2a
Louis Mangin <louis.mangin@beebryte.com>
2019-03-04 15:09:08 +0100

    Improved socket library

--- TP1/socketlib.c
+++ TP1/socketlib.c
@@ -9,18 +9,46 @@
 #include "socketlib.h"
 #define BUFFSIZE 50
 
-void send_buffer(char *buffer, int bufferSize, int packetSize, char *address,
-  int port) {
 
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
+int create_socket() {
+  int sz = 1;
+  int sock = socket(PF_INET, SOCK_STREAM, 0);
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &sz, 4);
+  return sock;
+}
+
+
+void bind_socket(int sock, char *address, int port) {
+  struct sockaddr_in my_addr;
+  my_addr.sin_family = AF_INET;
+  my_addr.sin_port = htons(port);
+  my_addr.sin_addr.s_addr = INADDR_ANY;
+  bzero(&(my_addr.sin_zero), 8);
+  if (bind (sock, (struct sockaddr *) &my_addr, sizeof(struct sockaddr)) != 0)
+    error(2, 0, "Cannot bind to %s:%d\n", address, port);
+  listen(sock, 4);
+}
+
+
+void connect_socket(int sock, char *address, int port) {
   struct sockaddr_in my_addr;
   my_addr.sin_family = AF_INET;
   my_addr.sin_port = htons(port);
   my_addr.sin_addr.s_addr = inet_addr(address);
   bzero(&(my_addr.sin_zero), 8);
-
   if (connect (sock, (struct sockaddr *) &my_addr, sizeof my_addr) != 0)
     error(1, 0, "Cannot connect to %s:%d\n", address, port);
+};
+
+
+int accept_socket(int sock) {
+  struct sockaddr_in from_addr;
+  unsigned int size = 0;
+  return accept(sock, (struct sockaddr *) &from_addr, &size);
+}
+
+
+void send_buffer(int sock, char *buffer, int bufferSize, int packetSize) {
 
   int max_idx = bufferSize / packetSize;
   for (int idx = 0; idx < max_idx; idx ++)
@@ -31,56 +59,44 @@ void send_buffer(char *buffer, int bufferSize, int packetSize, char *address,
   if (remaining != 0)
     write(sock, &buffer[max_idx * packetSize], remaining);
 
-  close(sock);
 };
 
 
-void send_int(int to_send, char *address, int port) {
-
-  char buf[BUFFSIZE];
-  sprintf(buf, "%d", to_send);
-  send_buffer(buf, BUFFSIZE, BUFFSIZE, address, port);
-
-};
-
-
-int recv_buffer(char *buffer, int bufferSize, int packetSize, char *address,
-  int port) {
-
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in my_addr;
-  my_addr.sin_family = AF_INET;
-  my_addr.sin_port = htons(port);
-  my_addr.sin_addr.s_addr = inet_addr(address);
-  bzero(&(my_addr.sin_zero), 8);
-
-  if (bind (sock, (struct sockaddr *) &my_addr, sizeof(struct sockaddr)) != 0)
-    error(2, 0, "Cannot bind to %s:%d\n", address, port);
-
-  listen(sock, 4);
-
-  struct sockaddr_in from_addr;
-  unsigned int size = 0;
-  int sock2 = accept(sock, (struct sockaddr *) &from_addr, &size);
+int recv_buffer(int sock, char *buffer, int bufferSize, int packetSize) {
 
   int count = -1, received = 0, idx = 0;
   while (count != 0) {
-    count = read(sock2, &buffer[idx * packetSize], packetSize);
+    count = read(sock, &buffer[idx * packetSize], packetSize);
     received += count;
     idx ++;
   }
 
   if (received > bufferSize)
-    error(3, 0, "Receiving too much from %s:%d\n", address, port);
+    error(3, 0, "Receiving too much from socket %d\n", sock);
 
   return received;
 };
 
 
+void send_int(int to_send, char *address, int port) {
+
+  int sock = create_socket();
+  connect_socket(sock, address, port);
+  char buf[BUFFSIZE];
+  sprintf(buf, "%d", to_send);
+  send_buffer(sock, buf, BUFFSIZE, BUFFSIZE);
+  close(sock);
+
+};
+
+
 int recv_int(char *address, int port) {
 
+  int sock1 = create_socket();
+  bind_socket(sock1, address, port);
+  int sock2 = accept_socket(sock1);
   char buf[BUFFSIZE];
-  recv_buffer(buf, BUFFSIZE, BUFFSIZE, address, port);
+  recv_buffer(sock2, buf, BUFFSIZE, BUFFSIZE);
   return atoi(buf);
 
 };
--- TP1/socketlib.h
+++ TP1/socketlib.h
@@ -1,9 +1,15 @@
-void send_buffer(char *buffer, int bufferSize, int packetSize, char *address,
-  int port);
+int create_socket();
+
+void bind_socket(int sock, char *address, int port);
+
+void connect_socket(int sock, char *address, int port);
+
+int accept_socket(int sock);
+
+void send_buffer(int sock, char *buffer, int bufferSize, int packetSize);
+
+int recv_buffer(int sock, char *buffer, int bufferSize, int packetSize);
 
 void send_int(int to_send, char *address, int port);
-
-int recv_buffer(char *buffer, int bufferSize, int packetSize, char *address,
-  int port);
 
 int recv_int(char *address, int port);
