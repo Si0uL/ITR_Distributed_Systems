bc8ea196d2155536fecf0756f03567279e1e3410
Louis Mangin <louis.mangin@beebryte.com>
2019-03-19 18:02:26 +0100

    Removing hard-coded sizes and files names

--- TP3/image_processing.c
+++ TP3/image_processing.c
@@ -36,16 +36,11 @@
 #include <string.h>
 #include <sys/time.h>
 
-#define W 943
-#define H 827
-#define IMAGE_IN "logo.pgm"
-#define IMAGE_OUT "logo2.pgm"
 #define CONV_MATRIX_SIZE 3
 
 /*  global variables */
-int image_in[H][W];
-int image_out[H][W];
 
+/*
 static const int conv_matrix_lines[CONV_MATRIX_SIZE][CONV_MATRIX_SIZE] = {
     { -3, 0, 3 },
     { -1, 0, 1 },
@@ -57,6 +52,7 @@ static const int conv_matrix_columns[CONV_MATRIX_SIZE][CONV_MATRIX_SIZE] = {
     { 0, 0, 0 },
     { 3, 1, 3 }
 };
+*/
 
 static const int conv_matrix_borders[CONV_MATRIX_SIZE][CONV_MATRIX_SIZE] = {
     { -1, -1, -1 },
@@ -64,126 +60,137 @@ static const int conv_matrix_borders[CONV_MATRIX_SIZE][CONV_MATRIX_SIZE] = {
     { -1, -1, -1 }
 };
 
-void read_image (int image[H][W], char file_name[], int *p_h, int *p_w,
+void read_image (int **image, char file_name[], int *p_height, int *p_width,
   int *p_levels) {
   FILE *fin;
-  int i, j, h, w, levels ;
+  int row, col;
   char buffer[80];
 
   /* open P2 image */
-  fin = fopen (IMAGE_IN, "r");
+  fin = fopen (file_name, "r");
   if (fin == NULL){
     printf ("file open error\n");
     exit(-1);
   }
   fscanf (fin, "%s", buffer);
-  if (strcmp (buffer, "P2")){
+  if (strcmp (buffer, "P2")) {
     printf ("the image format must be P2\n");
     exit(-1);
   }
   fgets (buffer, 80, fin);
   fgets (buffer, 80, fin);
-  fscanf (fin, "%d%d", &w, &h);
-  fscanf (fin, "%d", &levels);
-  printf ("image reading ... h = %d w = %d\n", h, w);
+  fscanf (fin, "%d%d", p_width, p_height);
+  fscanf (fin, "%d", p_levels);
+  printf ("image reading ... h = %d w = %d\n", *p_height, *p_width);
 
   /* pixels reading */
-  for (i = 0; i < h ; i++)
-    for (j = 0; j < w; j++)
-       fscanf (fin, "%d", &image[i][j]);
+  *image = calloc((*p_height) * (*p_width), sizeof(int));
+  for (row = 0; row < *p_height; row++)
+    for (col = 0; col < *p_width; col++)
+       fscanf (fin, "%d", &((*image)[row * (*p_width) + col]));
   fclose (fin);
 
-  *p_h = h;
-  *p_w = w;
-  *p_levels = levels;
   return;
 }
 
-void write_image (int image[H][W], char file_name[], int h, int w, int levels) {
+void write_image (int *image, char file_name[], int height, int width,
+  int levels) {
   FILE *fout;
-  int i, j;
+  int row, col;
 
   /* open the file */
-  fout=fopen(IMAGE_OUT,"w");
+  fout=fopen(file_name, "w");
   if (fout == NULL){
     printf ("file opening error\n");
     exit(-1);
   }
 
   /* header write */
-  fprintf(fout,"P2\n# test \n%d %d\n%d\n", w, h, levels);
+  fprintf(fout,"P2\n# test \n%d %d\n%d\n", width, height, levels);
   /* format P2, commentary, w x h points, levels */
 
   /* pixels writing*/
-  for (i = 0; i < h ; i++)
-    for (j = 0; j < w; j++)
-       fprintf (fout, "%d\n", image[i][j]);
+  for (row = 0; row < height; row++)
+    for (col = 0; col < width; col++)
+       fprintf (fout, "%d\n", image[row * width + col]);
 
   fclose (fout);
   return;
 }
 
-void convolution(int input[H][W], int output[H][W],
+void convolution(int *input, int *output, int height, int width,
   const int conv_matrix[CONV_MATRIX_SIZE][CONV_MATRIX_SIZE]) {
   // Recopy borders
-  for (int idx=0; idx < W; idx++) {
-    output[0][idx] = input[0][idx];
-    output[H-1][idx] = input[H-1][idx];
+  for (int idx=0; idx < width; idx++) {
+    output[0 * width + idx] = input[0 * width + idx];
+    output[(height-1) * width + idx] = input[(height-1) * width + idx];
   }
-  for (int idx=0; idx < H; idx++) {
-    output[idx][0] = input[idx][0];
-    output[idx][W-1] = input[idx][W-1];
+  for (int idx=0; idx < height; idx++) {
+    output[idx * width + 0] = input[idx * width + 0];
+    output[idx * width + width-1] = input[idx * width + width-1];
   }
 
   // Apply convolution elsewhere
   int row, col, r_conv, c_conv, sum;
   // Convolution offset
   int c_off = (int) CONV_MATRIX_SIZE / 2;
-  for (row = 1; row < H-1; row ++) {
-    for (col = 1; col < W-1; col ++) {
+  for (row = 1; row < height-1; row ++) {
+    for (col = 1; col < width-1; col ++) {
       sum = 0;
       for (r_conv = 0; r_conv < CONV_MATRIX_SIZE; r_conv ++) {
         for (c_conv = 0; c_conv < CONV_MATRIX_SIZE; c_conv ++) {
-          sum += input[row-c_off+r_conv][col-c_off+c_conv] * \
+          sum += input[(row-c_off+r_conv) * width + (col-c_off+c_conv)] * \
             conv_matrix[r_conv][c_conv];
         }
       }
-      output[row][col] = sum;
+      output[row * width + col] = sum;
     }
   }
   return;
 }
 
-void saturate(int image[H][W]) {
+void saturate(int *image, int height, int width) {
   int row, col;
-  for (row=0; row < H; row++) {
-    for (col=0; col < W; col++) {
-      if (image[row][col] < 0)
-        image[row][col] = 0;
-      else if (image[row][col] > 255)
-        image[row][col] = 255;
+  for (row=0; row < height; row++) {
+    for (col=0; col < width; col++) {
+      if (image[row * width + col] < 0)
+        image[row * width + col] = 0;
+      else if (image[row * width + col] > 255)
+        image[row * width + col] = 255;
     }
   }
   return;
 }
 
-int main(void) {
-  int h, w, levels ;
+int main(int argc, char const *argv[]) {
+
+  if (argc != 2) {
+    printf("Usage: %s <input_path>\n", argv[2]);
+    return 1;
+  }
+
+  int height, width, levels;
   struct timeval tdeb, tfin;
 
-  read_image (image_in, IMAGE_IN, &h, &w, &levels);
+  int *image_in;
+  read_image (&image_in, "logo.pgm", &height, &width, &levels);
+
+  int *image_out = calloc(height * width, sizeof(int));
 
   /* image processing */
   gettimeofday(&tdeb, NULL);
 
-  convolution(image_in, image_out, conv_matrix_borders);
-  saturate(image_out);
+  convolution(image_in, image_out, height, width, conv_matrix_borders);
+  saturate(image_out, height, width);
 
   gettimeofday(&tfin, NULL);
   printf ("computation time (microseconds): %ld\n",
-  (tfin.tv_sec - tdeb.tv_sec)*1000000 + (tfin.tv_usec - tdeb.tv_usec));
+    (tfin.tv_sec - tdeb.tv_sec)*1000000 + (tfin.tv_usec - tdeb.tv_usec));
 
-  write_image (image_out, IMAGE_OUT, h, w, levels);
+  write_image (image_out, "output.pgm", height, width, levels);
 
-  return 1;
+  free(image_in);
+  free(image_out);
+
+  return 0;
 }
